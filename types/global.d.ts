b7b826738e12dd673343aa4b4d35562bfaaca237
Lukas Nys <lukas.nys@otainsight.com>
2023-08-06 12:07:24 +0200

    add glint to global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,7 @@
 // Types for compiled templates
 declare module 'my-polaris-starter/templates/*' {
+  import '@glint/environment-ember-loose';
+  import '@glint/environment-ember-template-imports';
   import { TemplateFactory } from 'ember-cli-htmlbars';
 
   const tmpl: TemplateFactory;
